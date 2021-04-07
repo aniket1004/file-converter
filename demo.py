f8b1b60d8e4686ed13ba79ab92a386696c6d5201
aniket1004 <aniketdhole1004@gmail.com>
2021-04-07 15:48:19 +0530

    python file intial commit

---
+++ demo.py
@@ -0,0 +1,24 @@
+from docx2pdf import convert
+from pdf2docx import Converter
+import sys
+import os
+import win32com import client
+from traceback import print_exc
+from PIL import Image
+
+if sys.argv[1] == 'd-p':
+    convert(sys.argv[2])
+    print("convert success")
+    
+if sys.argv[1] == 'p-d':
+    cv = Converter(sys.argv[2])
+    open(sys.argv[3],"a")
+    cv.convert(sys.argv[3],start = 0, end = None)
+    cv.close()
+    print("convert success")
+if sys.argv[1] == 'i-i':
+    filename = sys.argv[2]
+    img = Image.open(filename)
+    img.save(sys.argv[3])
+    print("success")
+if (sys.argv[1] == 'p-d')
