8d93bdad9c3648bf3857689ca350a7198b121869
aniket1004 <aniketdhole1004@gmail.com>
2021-04-07 15:48:47 +0530

    document file intial commit

---
+++ doc.php
@@ -0,0 +1,120 @@
+<?php
+
+//import the converter class
+require('document_converter.php');
+if($_FILES){
+	$obj = new Document_converter();
+	
+	//call upload function and send the $_FILES, target folder and input name
+	$upload = $obj->upload_document($_FILES, 'uploads', 'fileToUpload');
+	if($upload){
+		$imageName = urlencode($upload[0]);
+		$imageType = urlencode($upload[1]);
+		
+		header('Location: convertdocument.php?imageName='.$imageName.'&imageType='.$imageType);
+	}
+}	
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>File Converter</title>
+		<meta charset="utf-8" />
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="">
+		<meta name="Keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<script>
+	function checkEmpty(){
+		var img = document.getElementById('fileToUpload').value;
+		if(img == ''){
+			alert('Please upload an image');
+			return false;
+		}
+		return true;
+	}
+</script>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+								</header>
+
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+									 
+									 <table width="500" align="center">
+		<tr><td align="center" style="background-color:thistle;">	<h2 align="center">Document Converter</h2></td></tr>
+		<tr><td align="center"><h4>Convert ant document to PDF,DOC,PPT</h4></td></th>
+		<tr>
+			<td align="center">
+				<form action="" enctype="multipart/form-data" method="post" onsubmit="return checkEmpty()" />
+					<input type="file" name="fileToUpload" id="fileToUpload" />
+					<input type="submit" value="Upload" />
+				</form>
+			</td>
+		</tr>
+	</table>
+									</div>
+								</section>
+
+						
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner" style="background-color:antiquewhite;">
+
+							<!-- Search -->
+								<section id="search" class="alt">
+								<h1> Tools</h1>
+								</section>
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Converter</h2>
+									</header>
+									<ul>
+										<li><a href="index.php">Image</a></li>
+									</ul>
+									<ul>
+										<li><a href="doc.php">Document</a></li>
+									</ul>
+								</nav>
+
+						
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright"><b>&copy; 2021</b></p>
+								</footer>
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
