a6a5a66bb8ae464ebf8ce44821984979ac5393fd
aniket1004 <aniketdhole1004@gmail.com>
2021-04-09 11:43:10 +0530

    python logic

--- demo.py
+++ demo.py
@@ -2,9 +2,8 @@ from docx2pdf import convert
 from pdf2docx import Converter
 import sys
 import os
-import win32com import client
-from traceback import print_exc
 from PIL import Image
+from fpdf import FPDF
 
 if sys.argv[1] == 'd-p':
     convert(sys.argv[2])
@@ -21,4 +20,21 @@ if sys.argv[1] == 'i-i':
     filename = sys.argv[2]
     img = Image.open(filename)
     img.save(sys.argv[3])
+    print("success")
+
+if sys.argv[1] == 'i-b':
+    filename = sys.argv[2]
+    img = Image.open(filename).save(sys.argv[3])
+    print("success")
+    
+if sys.argv[1] == 't-p':
+    filename = sys.argv[2]
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial",size = 14)
+    f = open(filename,"r")
+    for x in f:
+        pdf.cell(200,10,txt = x,ln = 1,align = 'C')
+        
+    pdf.output(sys.argv[3])
     print("success")
