80b28fda74746a554bc27941c42b39fc71b655e3
aniket1004 <aniketdhole1004@gmail.com>
2021-04-09 11:49:36 +0530

    deleted beta

--- beta/download.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$file_path = $_GET['filepath'];
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($file_path) . "\""); 
-readfile($file_path); 
-
-?>
